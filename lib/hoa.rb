d7810f83b44656996b332c95603e0eda500853e1
Kat Maldon <katharinamaldon@gmail.com>
2020-03-28 23:29:08 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -6,9 +6,6 @@ BASE_HOA = {
 
 
 def add_character(show, name)
-  # Write your implementation here
-  # Should return the array of the 'show' argument
-  
   if !BASE_HOA[show].include?(name)
   	BASE_HOA[show] << name
   end
